ce22e0677aecf3c9990808b170a79e5b2afc2067
Alexandre Lefevre <alefevre@smartwavesa.com>
2018-10-02 14:31:08 +0200

    init backend with spring boot initializer

---
+++ cryptlaser-backend/src/main/java/com/wodric/cryptlaserbackend/CryptlaserBackendApplication.java
@@ -0,0 +1,12 @@
+package com.wodric.cryptlaserbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptlaserBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptlaserBackendApplication.class, args);
+	}
+}
---
+++ cryptlaser-backend/src/test/java/com/wodric/cryptlaserbackend/CryptlaserBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wodric.cryptlaserbackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CryptlaserBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
